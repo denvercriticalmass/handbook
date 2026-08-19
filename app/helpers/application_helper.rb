9dd40135aaa162d053c451401e0f5c5d09713dee
Joseph Sak <joseph.sak@gusto.com>
2026-08-19 11:23:59 -0600

    Say what a destructive control does before it runs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def suspension_warning(user)
+    if user.active?
+      "Suspend #{user.name}? They are signed out and can't sign back in."
+    else
+      "Reinstate #{user.name}?"
+    end
+  end
 end
