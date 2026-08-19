6f48167f3da1ec6ae43a0d80f8f212e566f18b4a
Joseph Sak <joseph.sak@gusto.com>
2026-08-18 23:37:24 -0600

    Drop the cop_helper comment

--- spec/cop_helper.rb
+++ spec/cop_helper.rb
@@ -1,6 +1,4 @@
 require "rubocop"
 require "rubocop/rspec/support"
 
-# Every cop in .rubocop/cops, so adding one needs no wiring here. No Rails, so
-# these run in a fraction of a second.
 Dir[File.expand_path("../.rubocop/cops/*.rb", __dir__)].sort.each { require it }
